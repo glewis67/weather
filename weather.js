ae7510162f4464a8aee9176c928df5d0dc0583bd
glewis67 <84549006+glewis67@users.noreply.github.com>
2021-12-15 17:22:35 -0600

    add files

--- weather.js
+++ weather.js
@@ -1,115 +1,116 @@
-// for (var i = 0; i < localStorage.length; i++) {
-//   console.log(cityName);
-// 	var city = localStorage.length(i);
-
-// 	var cityName = (city);
-
-// 	cityName.append();
+function searchCity(searchingCity){
  
-//   }
-
-//   var searchButton = $(".button");
-
-
-
-
-var apiKey = "a6a3d14f7393320c892d52e7e25b0a02";
-var baseURL1 =
-  "https://api.openweathermap.org/data/2.5/weather?q=austin&appid=" + apiKey;
-
-fetch(baseURL1)
-  .then(function (res) {
-    return res.json();
-  })
-  .then(function (res) {
-	  console.log(res);
-    var cityEl = document.querySelector("#city");
-	cityEl.textContent = res.name;
-    var lat = res.coord.lat;
-    var lon = res.coord.lon;
-    var baseURL2 =
+  
+  var apiKey = "a6a3d14f7393320c892d52e7e25b0a02";
+  var baseURL1 =
+    "https://api.openweathermap.org/data/2.5/weather?q="+searchingCity+"&appid=" + apiKey;
+  
+    fetch(baseURL1)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (res) {
+      console.log(res);
+      $("#city").text(res.name)
+      var lat = res.coord.lat;
+      var lon = res.coord.lon;
+      var baseURL2 =
       "https://api.openweathermap.org/data/2.5/onecall?lat=" +
       lat +
       "&lon=" +
       lon +
       "&appid=" +
-      apiKey + "&units=imperial";
-
-    fetch(baseURL2)
+      apiKey + "&units=imperial"; 
+      fetch(baseURL2)
       .then(function (res) {
         return res.json();
       })
       .then(function (data) {
         console.log(data);
-		var tempEl = document.querySelector("#temp");
-		tempEl.textContent = data.current.temp
-      });
-
-      $.ajax({
-        url: weatherURL,
-        method: "GET",
-    }).then(function (res) {
-        var uvI = res.value;
+        $("#temp").text( data.current.temp + " °F");
+        $("#Humidity").text("Humidity: " + data.current.humidity + " %");
+          $("#Wind").text("Wind Speed: " + data.current.wind_speed + " MPH");
+          $("#UV").text("UV Index: " + data.current.uvi);
+          $("<img>").appendTo("#img").attr("src", `https://openweathermap.org/img/w/${res.weather[0].icon}.png`); //ICON 
+  
+          for (let i = 1; i < 6; i++){
+            $("#date" +i).text(moment().add(i, "days").format("MM/DD/YYYY"));     //Date at top of cards
+            $("<img>").appendTo("#icon"+ i).attr("src", `https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png`); //ICON
+            $("#temp" + i).text("Temperature: " + data.daily[i].temp.day + " °F");
+            $("#humidity" + i).text("Humidity: " + data.daily[i].humidity + " %");
+      }
+      })
        
-        $(".city").text(city + " " + moment().format('l'));
-        $("<img>").appendTo(".city").attr("src", `https://openweathermap.org/img/w/${response.list[0].weather[0].icon}.png`); //ICON
-        $(".temp").text("Temperature: " + response.list[0].main.temp + " °F");
-        $(".humidity").text("Humidity: " + response.list[0].main.humidity + " %");
-        $(".wind").text("Wind Speed: " + response.list[0].wind.speed + " MPH");
-        $(".uvIndex").text("UV Index: " + uvI);
-        console.log(response);
-        
-        
-        var Date = newDate();
-        var numberOfDaysToAdd = 6;
-
-        var dd = Date.getDate();
-        var mm = Date.getMonth() + 1;
-        var y =  Date.getFullYear();
-
-        var FormattedDate = dd + '/'+ mm + '/'+ y;
-
-        var myDate = new Date(new Date().getTime()+(5*24*60*60*1000));
-        
-        for (let i = 1; i < 6; i++){
-          $(".date" +[i]).text(moment().add(i, "days").format('l'));     //Date at top of cards
-          $(".icon"+ [i]).attr("src", `https://openweathermap.org/img/w/${response.list[(i*8)-1].weather[0].icon}.png`); //ICON
-          $(".temp" + [i]).text("Temperature: " + response.list[(i*8)-1].main.temp + " °F");
-          $(".humidity" + [i]).text("Humidity: " + response.list[(i*8)-1].main.humidity + " %");
-    }
+        });
+}
+$("#search-button").on("click", function(){
+  var city= $("#search-value").val()
+  searchCity(city)
+  saveCity(city)
+})
+  
     
-    let lat = response.city.lat;
-    let lon = response.city.lon;
+        
+       
 
-    getWeather()
 
-    {
-        let cities = JSON.parse(localStorage.getItem('cities'));
+function saveCity(city){
+  $("#history").html()
+  let cities = JSON.parse(localStorage.getItem('cities'));
         if (!cities) {
-            cities = [];
-            localStorage.setItem('cities', JSON.stringify(cities));
+             cities = [];
+            
         }
-        for (let i = 0; i < cities.length; i++) {
+cities.push(city)
+localStorage.setItem('cities', JSON.stringify(cities));
+
+       for (let i = 0; i < cities.length; i++) {
             let button = $("<button>");
             let city = cities[i];
-            button.click(function(event){
-                event.preventDefault();
-                console.log(this.textContent);
-                getWeather(this.textContent);
-            });
-            button.appendTo(".nav").attr("class", "btn btn-outline-info pastCitySearch col-12").text(city);
+             button.click(function(event){
+             event.preventDefault();
+               searchCity(city)
+             });
+            button.appendTo("#history").attr("class", "btn btn-outline-info pastCitySearch col-12").text(city);
+         }
+}
+
+    function loadCities(){
+      let cities = JSON.parse(localStorage.getItem('cities'));
+        if (!cities) {
+             cities = [];
+            
         }
+
+localStorage.setItem('cities', JSON.stringify(cities));
+
+       for (let i = 0; i < cities.length; i++) {
+            let button = $("<button>");
+            let city = cities[i];
+             button.click(function(event){
+             event.preventDefault();
+               searchCity(city)
+             });
+            button.appendTo("#history").attr("class", "btn btn-outline-info pastCitySearch col-12").text(city);
+         }
+
+    }     
+  loadCities()  
+  
+
+  //   getWeather()
+
+  //   {
+  //      
+  
       
         
-    }
-  }); 
-  });
+  //   }
+  // }); 
+  // });
 
  
        
 
-// if(length>0 && length<6) {
-//   $(".containerDay1").append(
-//     (day.dt).format("M/DD/YYYY")]
-//   )
+
   
